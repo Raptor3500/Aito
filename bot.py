3aac1cf2fcb29881fc8f93b33e43e9609b6c37fb
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-10-15 17:58:27 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -138,7 +138,7 @@ async def prune(ctx, number, *args):
         mgs.append(x)
     await bot.delete_messages(mgs)
     await asyncio.sleep(1)
-    await bot.say(channel, "I have deleted" + mesg + "messages")
+    await bot.send_message.channel("I have deleted" + mesg + "messages")
     await bot.delete_messages("I have deleted" + mesg + "messages")
     
 @bot.command()

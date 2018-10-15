ef593d12dab2b286c412f59f1cd11caa67b92572
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-10-15 17:18:29 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -131,6 +131,7 @@ async def rate(str : str):
 @bot.command(pass_context=True)
 async def prune(ctx, number, *args):
   if ctx.message.author.id in ownerID:
+    mesg = ' '.join(args)
     mgs = [] #Empty list to put all the messages in the log
     number = int(number) #Converting the amount of messages to delete to an integer
     async for x in bot.logs_from(ctx.message.channel, limit = number):

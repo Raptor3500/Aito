120ba4934fdc14b09147976e397cba54f496a8c6
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-10-19 08:36:05 -0400

    Create delete.py

---
+++ delete.py
@@ -0,0 +1,8 @@
+import discord
+
+class delete():
+    def __init__(self, bot):
+        self.bot = bot
+        
+        async def on_message(self,message):
+            if message.author.id = '436294612521582603'

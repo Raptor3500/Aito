ae697d742bc94fc5925f88f616ac67d7bf2e0335
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-10-19 08:51:06 -0400

    Update delete.py

--- delete.py
+++ delete.py
@@ -5,9 +5,8 @@ class delete():
         self.bot = bot
         
         async def on_message(self,message,ctx):
-            if ctx.message.author.id == '436294612521582603':
-               if ' ' in message.content:
-                await self.bot.delete_message(ctx.message)
+            if message.author.id == '436294612521582603' and ' ' in message.content:
+                await self.bot.delete_message(message.content)
                 
 def setup(bot):
     bot.add_cog(delete(bot))

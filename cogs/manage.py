a9c20cdc898363f9e8b37497fc9d1b4f7fa9b4ae
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-10-19 19:50:46 -0400

    Update manage.py

--- cogs/manage.py
+++ cogs/manage.py
@@ -9,63 +9,63 @@ class manage():
     def __init__(self, bot):
         self.bot = bot
 
- @commands.command(pass_context=True)
- async def load(self, ctx, extension_name : str):
-     """Loads an extension."""
-     extension_name = extension_name.strip()
-     if "cogs." not in extension_name:
-         extension_name = "cogs." + extension_name # nico load Owner => nico load cogs.Owner
-         if ctx.message.author.id in ownerID:
-             try:
-                 self.bot.load_extension(extension_name)
-                 embed = discord.Embed(title=None, description=None, color=OK)
-                 embed.add_field(name=extension_name, value="Cog loaded.")
-                 await self.bot.say(embed=embed)
-             except (AttributeError, ImportError) as e:
-                 embedError = discord.Embed(title=None, description=None, color=Error)
-                 embedError.add_field(name='Cog Error', value="```py\n{}: {}\n```".format(type(e).__name__, str(e)), inline=True)
-                 await self.bot.say(embed=embedError)
-                 return
-         else:
-             noperm = discord.Embed(title=None, description=None, color=Error)
-             noperm.add_field(name='Error', value='Are you sure you have permission to load ' + extension_name, inline=True)
-             await self.bot.say(embed=noperm)
+    @commands.command(pass_context=True)
+    async def load(self, ctx, extension_name : str):
+        """Loads an extension."""
+        extension_name = extension_name.strip()
+        if "cogs." not in extension_name:
+            extension_name = "cogs." + extension_name # nico load Owner => nico load cogs.Owner
+            if ctx.message.author.id in ownerID:
+                try:
+                    self.bot.load_extension(extension_name)
+                    embed = discord.Embed(title=None, description=None, color=OK)
+                    embed.add_field(name=extension_name, value="Cog loaded.")
+                    await self.bot.say(embed=embed)
+                except (AttributeError, ImportError) as e:
+                    embedError = discord.Embed(title=None, description=None, color=Error)
+                    embedError.add_field(name='Cog Error', value="```py\n{}: {}\n```".format(type(e).__name__, str(e)), inline=True)
+                    await self.bot.say(embed=embedError)
+                    return
+            else:
+                noperm = discord.Embed(title=None, description=None, color=Error)
+                noperm.add_field(name='Error', value='Are you sure you have permission to load ' + extension_name, inline=True)
+                await self.bot.say(embed=noperm)
 
- @commands.command(pass_context=True)
- async def unload(self, ctx, extension_name : str):
-     """Unloads an extension."""
-     extension_name = extension_name.strip()
-     if "cogs." not in extension_name:
-         extension_name = "cogs." + extension_name # nico unload Owner => nico unload cogs.Owner
-         if ctx.message.author.id in ownerID:
-             if "Owner" in extension_name:
-                 embed = discord.Embed(title=None, description=None, color=Error)
-                 embed.add_field(name='Error', value=extension_name + " can't be unloaded.", inline=False)
-                 await self.bot.say(embed=embed) # This obviously stops cogs.Owner from being disabled.
-             else:
-                 self.bot.unload_extension(extension_name)
-                 embed = discord.Embed(title=None, description=None, color=OK)
-                 embed.add_field(name=extension_name, value="Cog unloaded.")
-                 await self.bot.say(embed=embed)
-         else:
-             noperm = discord.Embed(title=None, description=None, color=Error)
-             noperm.add_field(name='Error', value='Are you sure you have enough permission to unload ' + extension_name, inline=True)
-             await self.bot.say(embed=noperm)
+    @commands.command(pass_context=True)
+    async def unload(self, ctx, extension_name : str):
+        """Unloads an extension."""
+        extension_name = extension_name.strip()
+        if "cogs." not in extension_name:
+            extension_name = "cogs." + extension_name # nico unload Owner => nico unload cogs.Owner
+            if ctx.message.author.id in ownerID:
+                if "Owner" in extension_name:
+                    embed = discord.Embed(title=None, description=None, color=Error)
+                    embed.add_field(name='Error', value=extension_name + " can't be unloaded.", inline=False)
+                    await self.bot.say(embed=embed) # This obviously stops cogs.Owner from being disabled.
+                else:
+                    self.bot.unload_extension(extension_name)
+                    embed = discord.Embed(title=None, description=None, color=OK)
+                    embed.add_field(name=extension_name, value="Cog unloaded.")
+                    await self.bot.say(embed=embed)
+            else:
+                noperm = discord.Embed(title=None, description=None, color=Error)
+                noperm.add_field(name='Error', value='Are you sure you have enough permission to unload ' + extension_name, inline=True)
+                await self.bot.say(embed=noperm)
                 
-@commands.command(pass_context=True)
-async def bfmode(self, ctx, str : str)
-    if ctx.message.author.id in ownerID:
-        str = str.strip()
-        if 'agent' in str
-            with open('sora.png') as s:
-                await self.bot.edit_profile(avatar=s.read())
-                await self.bot.change_nick('Sora')
-                await self.bot.change_presence('with agents ;)))')
-        if 'reset' in str
-            with open('Aito Isshoni pfp.png') as a:
-                await self.bot.edit_profile(avatar=a.read())
-                await self.bot.change_nick(' ')
-                await self.bot.change_presence(' ')
+   @commands.command(pass_context=True)
+   async def bfmode(self, ctx, str : str)
+       if ctx.message.author.id in ownerID:
+           str = str.strip()
+           if 'agent' in str
+               with open('sora.png') as s:
+                   await self.bot.edit_profile(avatar=s.read())
+                   await self.bot.change_nick('Sora')
+                   await self.bot.change_presence('with agents ;)))')
+           if 'reset' in str
+               with open('Aito Isshoni pfp.png') as a:
+                   await self.bot.edit_profile(avatar=a.read())
+                   await self.bot.change_nick(' ')
+                   await self.bot.change_presence(' ')
                 
 
 

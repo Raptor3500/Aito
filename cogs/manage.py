cb62bcaf90fde0d1e9f87e30f0edcfbf2875009e
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-10-19 23:30:21 -0400

    Update manage.py

--- cogs/manage.py
+++ cogs/manage.py
@@ -61,12 +61,10 @@ class manage():
                 with open('Sora.png', 'rb') as f:
                     await self.bot.edit_profile(avatar=f.read())
                     await self.bot.change_nickname(me, 'Sora')
-                    await self.bot.change_presence('with agents ;)))')
             if 'reset' in str:
                 with open('Aito Isshoni pfp.png', 'rb') as f:
                     await self.bot.edit_profile(avatar=f.read())
                     await self.bot.change_nickname(me, ' ')
-                    await self.bot.change_presence()
                 
 
 

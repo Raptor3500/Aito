010be96d6cc29c65f88dde9e533fc0f6091decad
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-10-22 10:36:35 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -156,7 +156,7 @@ async def setmygame(ctx, *args):
     await bot.say("you are now playing " + mesg)
 
 @bot.command(pass_context=True)
-async def bfmode(ctx, str : str)
+async def bfmode(ctx, str : str):
   if ctx.message.author.id in ownerID:
     if 'reset' in str:
       with open('new aito isshoni pfp.jpg', 'rb') as f:

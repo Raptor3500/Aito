681fcdae391336487e3bb22f8a7e086e4d168670
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-11-08 12:47:08 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ import random
 import youtube_dl
 
 startup_extensions = [
-  'cogs.message','cogs.manage','cogs.richpresence'
+  'cogs.message','cogs.manage',
 ]
 
 bot = commands.Bot(command_prefix='aito ')

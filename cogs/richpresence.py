a53afcac8c20ae5e3d7a1259f5eac66c0e0ec130
Xenzai <37379722+Raptor3500@users.noreply.github.com>
2018-11-08 12:35:01 -0500

    Create richpresence.py

---
+++ cogs/richpresence.py
@@ -0,0 +1,15 @@
+from pypresence import Presence
+import time
+
+class richpresence():
+    def __init__(self, bot):
+        self.bot = bot
+
+client_id = '497480630687367188'
+RPC = Presence(client_id)  # Initialize the client class
+RPC.connect() # Start the handshake loop
+
+print(RPC.update(state="Giovanni x Natsuki", details="Xenzai x Rem/ Nakune x Koneko Btw Don't judge me!"))  # Set the presence
+
+while True:  # The presence will stay on as long as the program is running
+    time.sleep(15) # Can only update rich presence every 15 seconds
